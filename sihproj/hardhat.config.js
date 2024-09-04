bba294325f6ba8ee56ff1e08a56e8aef0a643a37
VB5475 <vatsalb5475@gmail.com>
2024-09-04 12:16:40 +0530

    added sih proj

---
+++ sihproj/hardhat.config.js
@@ -0,0 +1,68 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+// module.exports = {
+//    solidity: “0.8.9”,
+//    defaultNetwork: “mumbai”,
+//    networks: {
+//      hardhat: {},
+//      mumbai: {
+//        url: API_URL,
+//        accounts: [`0x${PRIVATE_KEY}`],
+//      },
+//    },
+// };
+
+module.exports = {
+ solidity: {
+  version: "0.8.9",
+  settings: {
+   optimizer: {
+    enabled: true,
+    runs: 200,
+   },
+  },
+ },
+ networks: {
+  hardhat: {},
+  mumbai: {
+   url: "https://polygon-mumbai.api.onfinality.io/public",
+
+   // url: API_URL,
+   accounts: [`0x${PRIVATE_KEY}`],
+  },
+ },
+};
+
+// module.exports = {
+//   solidity: {
+//     version: "0.8.9",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200,
+//       },
+//     },
+//   },
+//   paths: {
+//     artifacts:'./src/artifacts'
+//   },
+//   networks: {
+//     hardhat: {},
+//     mumbai: {
+//       url:"https://rpc-mumbai.maticvigil.com",
+
+//       // url: API_URL,
+//       accounts: [`0x${PRIVATE_KEY}`],
+//     },
+//   },
+// };
+
+// 0xdc7E32f619Ed90ebEd72Fe8d94EA4ed34C9e32Ce
+// liquid else trouble tank sugar sea sleep easy profit snack duty clog
+
+// 0x51469A96044387786C6596F8bb98faC647cd502c
---
+++ sihproj/scripts/mint-nft.js
@@ -0,0 +1,73 @@
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+
+const minter = async (contractAddress, jsoncid) => {
+ const API_URL = "https://rpc-mumbai.maticvigil.com";
+
+ const PUBLIC_KEY = "0x47Fd3622841bd24721d94EA240bD70Ee431bBe80";
+ const PRIVATE_KEY =
+  "d1376117b5075b684b3e9730d048945f0c8c4c554b1ba92cb2712b3cd848ec96";
+ const web3 = createAlchemyWeb3(API_URL);
+
+ const contract = require("../artifacts/contracts/NFT2.0.sol/NFT20.json");
+
+ console.log(JSON.stringify(contract.abi));
+
+ const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+ try {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest");
+
+  const tx = {
+   from: PUBLIC_KEY,
+   to: contractAddress,
+   nonce: nonce,
+   gas: 500000,
+   data: nftContract.methods.safeMint(PUBLIC_KEY, jsoncid).encodeABI(),
+  };
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+
+  const signedTx = await signPromise;
+
+  const transactionReceipt = await new Promise((resolve, reject) => {
+   web3.eth.sendSignedTransaction(
+    signedTx.rawTransaction,
+    function (err, hash) {
+     if (err) {
+      console.log(
+       "Something went wrong when submitting your transaction:",
+       err
+      );
+      reject(err); // Reject the promise if there's an error
+     } else {
+      console.log(
+       "The hash of your transaction is: ",
+       hash,
+       "\nCheck Alchemy's Mempool to view the status of your transaction!"
+      );
+
+      // Attempt to get the tokenId by calling the safeMint function
+      nftContract.methods
+       .safeMint(PUBLIC_KEY, jsoncid)
+       .call({ from: PUBLIC_KEY }, (error, result) => {
+        if (!error) {
+         console.log("The tokenId of the minted NFT is: " + result);
+         resolve({ result, hash }); // Resolve the promise with the values
+        } else {
+         console.error("Error getting tokenId: " + error.message);
+         reject(error); // Reject the promise if there's an error
+        }
+       });
+     }
+    }
+   );
+  });
+
+  return transactionReceipt;
+ } catch (error) {
+  console.error("Error:", error);
+  throw error; // Throw the error if there's an error
+ }
+};
+
+module.exports = minter;
